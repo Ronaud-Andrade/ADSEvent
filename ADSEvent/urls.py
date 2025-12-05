7494094567e6f5ecf30b4f96bdee3be8551b2147
Chapadin <ronaudandrade108@gmail.com>
2025-12-05 20:56:30 -0300

    Apresentação Final

--- ADSEvent/urls.py
+++ ADSEvent/urls.py
@@ -25,8 +25,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('core.Api.v1.router')),
-    path('api/v1/', include('core.api.v1.router')), # Incluindo as rotas da API v1
+    path('api/v1/', include('core.api.v1.router')),
+    #path('api/v1/', include('core.api.v1.router')), # Incluindo as rotas da API v1
 
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='category_list'), name='logout'),
--- core/api/v1/serializers.py
+++ core/api/v1/serializers.py
@@ -1,17 +1,17 @@
-from rest_framework import serializers  # Importa o módulo de serializers do DRF.
-from core.models import CategoryEvent, Events, Subscribe  # Importa os modelos que serão serializados.
+from rest_framework import serializers  #Importa o módulo de serializers do DRF.
+from core.models import CategoryEvent, Events, Subscribe  #Importa os modelos que serão serializados.
 
-class CategorySerializer(serializers.ModelSerializer):  # Cria um serializer baseado no modelo CategoryEvent.
-    class Meta:  # Define configurações internas do serializer.
-        model = CategoryEvent  # Indica qual modelo o serializer representa.
-        fields = '__all__'  # No caso de categoria, só vai ser pego um campo.
+class CategorySerializer(serializers.ModelSerializer):  #Cria um serializer baseado no modelo CategoryEvent.
+    class Meta:  #Define configurações internas do serializer.
+        model = CategoryEvent  #Indica qual modelo o serializer representa.
+        fields = '__all__'  #No caso de categoria, só vai ser pego um campo.
 
-class EventSerializer(serializers.ModelSerializer):  # Cria um serializer para o modelo Events.
-    class Meta:  # Configurações internas do serializer.
-        model = Events  # Modelo vinculado ao serializer.
-        fields = '__all__'  # Usa todos os campos do modelo.
+class EventSerializer(serializers.ModelSerializer):  #Cria um serializer para o modelo Events.
+    class Meta:  #Configurações internas do serializer.
+        model = Events  #Modelo vinculado ao serializer.
+        fields = '__all__'  #Usa todos os campos do modelo.
 
-class SubscribeSerializer(serializers.ModelSerializer):  # Cria um serializer para o modelo Subscribe.
-    class Meta:  # Configurações internas do serializer.
-        model = Subscribe  # Define o modelo usado.
-        fields = '__all__'  # Inclui todos os campos automaticamente.
+class SubscribeSerializer(serializers.ModelSerializer):  #Cria um serializer para o modelo Subscribe.
+    class Meta:  #Configurações internas do serializer.
+        model = Subscribe  #Define o modelo usado.
+        fields = '__all__'  #Inclui todos os campos automaticamente.
